1f9da5fb9ce5364d54f84df3230e08a36fc23d6a
wjl <411046642@qq.com>
2021-05-12 08:43:44 +0800

    去掉react-image-magnify的依赖

--- src/views/item/ProductImage.js
+++ src/views/item/ProductImage.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import {Image, Row, Space} from 'antd';
-import ReactImageMagnify from 'react-image-magnify';
+// import ReactImageMagnify from 'react-image-magnify';
 //这里是处理传入进来的图片模块
 class ProductImage extends React.Component{
     constructor(props){
@@ -13,7 +13,7 @@ class ProductImage extends React.Component{
     render(){
         return(
             <div>
-                <ReactImageMagnify style={{width:200, height:200, zIndex: 10}} {...{
+                {/* <ReactImageMagnify style={{width:200, height:200, zIndex: 10}} {...{
                         smallImage: {
                             alt: '看不到图片',
                             src: 'http://image.uc.cn/s/wemedia/s/upload/2020/e3466f09e4bc2b32558be930245a2454.jpg',
@@ -26,8 +26,8 @@ class ProductImage extends React.Component{
                             height: 800
                         }
                     }}    
-                />
-                {/* <Image src={this.state.ImageList[0]} preview={false} width={400}/> */}
+                /> */}
+                <Image src={this.state.ImageList[0]} preview={false} width={400}/>
                 <Row>
                     <Space size={24}>
                         <Image src={'http://image.uc.cn/s/wemedia/s/upload/2020/e3466f09e4bc2b32558be930245a2454.jpg'} preview={false} width={60}> </Image>
--- src/views/user/UserModifyInfo.js
+++ src/views/user/UserModifyInfo.js
@@ -1,11 +1,80 @@
 import React from 'react';
-
+import { Form, Input, Select, Button, Space, DatePicker, Avatar } from 'antd';
+import { UserOutlined,UploadOutlined } from '@ant-design/icons';
+const { Option } = Select;
 class UserModifyInfo extends React.Component{
-
+    onFinish = values => {
+        console.log('Received values of form: ', values);
+    };
     render() {
         return(
             <div>
-                <div>这里是测试</div>
+                <Form name="complex-form" onFinish={this.onFinish} labelCol={{ span: 4 }} wrapperCol={{ span: 16 }} style={{textAlign:'left'}}>
+                    <Form.Item label="头像">
+                        <Space size={20}>
+                            <Avatar style={{ backgroundColor: '#f56a00', verticalAlign: 'middle' }} size="large" gap={2} icon={<UserOutlined />}>
+                                无法显示    
+                            </Avatar>
+                            <Button icon={<UploadOutlined />}>上传个人头像</Button>
+                        </Space>
+                    </Form.Item>
+                    <Form.Item label="用户昵称">
+                        <Space>
+                        <Form.Item
+                            name="username"
+                            noStyle
+                            rules={[{ required: true, message: 'Username is required' }]}
+                        >
+                            <Input style={{ width: 160 }} placeholder="个人昵称" />
+                        </Form.Item>
+                        </Space>
+                    </Form.Item>
+                    <Form.Item label="地址">
+                        <Input.Group compact>
+                        <Form.Item
+                            name={['address', 'province']}
+                            noStyle
+                            rules={[{ required: true, message: 'Province is required' }]}
+                        >
+                            <Select placeholder="省份">
+                            <Option value="Beijing">北京市</Option>
+                            <Option value="Shanghai">上海市</Option>
+                            <Option value="Guangdong">广东省</Option>
+                            <Option value="Shenzhen">河北省</Option>
+                            </Select>
+                        </Form.Item>
+                        <Form.Item>
+                            <Select placeholder="市区">
+                            <Option value="Beijing">北京市</Option>
+                            <Option value="Shanghai">上海市</Option>
+                            <Option value="Shenzhen">深圳市</Option>
+                            <Option value="Shijiazhuang">石家庄</Option>
+                            </Select>
+                        </Form.Item>
+                        <Form.Item
+                            name={['address', 'street']}
+                            noStyle
+                            rules={[{ required: true, message: '请输入详细地址' }]}
+                        >
+                            <Input style={{ width: '40%' }} placeholder="详情地址" />
+                        </Form.Item>
+                        </Input.Group>
+                    </Form.Item>
+                    <Form.Item label="出生日期" style={{ marginBottom: 0 }}>
+                        <Form.Item
+                        name="year"
+                        rules={[{ required: true }]}
+                        style={{ display: 'inline-block', width: 'calc(50% - 8px)' }}
+                        >
+                        <DatePicker renderExtraFooter={() => 'extra footer'} />
+                        </Form.Item>
+                    </Form.Item>
+                    <Form.Item label=" " colon={false}>
+                        <Button type="primary" htmlType="submit">
+                        保存修改
+                        </Button>
+                    </Form.Item>
+                    </Form>
             </div>
         );
     }
