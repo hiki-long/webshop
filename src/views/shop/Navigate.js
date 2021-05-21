1b680c88f038201b2020d0c322bb572efe04edfa
wjl <411046642@qq.com>
2021-05-21 16:08:40 +0800

    添加菜单栏

--- src/views/shop/Navigate.js
+++ src/views/shop/Navigate.js
@@ -28,11 +28,34 @@ class Navigate extends React.Component{
                         <Menu.Item key="/">
                             <Link to='/'>主页</Link>
                         </Menu.Item>
+                        <Menu.Item key="login">
+                            <Link to="/login">登录</Link>
+                        </Menu.Item>
+                        <Menu.Item key="itemUpload">
+                            <Link to="/itemUpload">商品上传页</Link>    
+                        </Menu.Item>
                         <Menu.Item key="itemList">
-                            <Link to='/itemList'>商品列表</Link>   
+                            <Link to="/itemList">商品列表</Link>   
+                        </Menu.Item>
+                        <Menu.Item>
+                            <Link to="/user">用户中心</Link>
+                        </Menu.Item>
+                        <Menu.Item>
+                            <Link to="/forget">忘记密码</Link>
+                        </Menu.Item>
+                        <Menu.Item>
+                            <Link to="/test">测试页面</Link>
                         </Menu.Item>
                     </Menu>
-                
+                    {/* <Input.Search 
+                        style={{textAlign:"center", marginLeft:"280px"}}
+                        placeholder="请输入想要搜索的商品"
+                        allowClear
+                        enterButton="查找"
+                        size="large"/>
+                    <Avatar style={{ backgroundColor: '#f56a00'}} size="large" icon={<UserOutlined />}>
+                        无法显示    
+                    </Avatar> */}
                {/* <Row>
                     <Col span={24}>
                         <nav className="NavbarItems" >
@@ -48,17 +71,8 @@ class Navigate extends React.Component{
                                 })}
                             </ul>
                             <ul>
-                                <Input.Search 
-                                    style={{textAlign:"center", marginLeft:"280px"}}
-                                    placeholder="请输入想要搜索的商品"
-                                    allowClear
-                                    enterButton="查找"
-                                    size="large"/>
                             </ul>
                             <ul>
-                                <Avatar style={{ backgroundColor: '#f56a00'}} size="large" icon={<UserOutlined />}>
-                                    无法显示    
-                                </Avatar>
                             </ul>
                             <ul>
                                 <span>用户昵称</span>
