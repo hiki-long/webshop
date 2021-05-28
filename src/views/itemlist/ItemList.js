aafc3db7d9505bbe37f089c85c789483aaf4a0a5
yuu <shiratsuyuclassyuudachi@protonmail.com>
2021-05-28 09:12:25 +0800

    add fetch to OneItenInfo

--- src/views/itemlist/ItemList.js
+++ src/views/itemlist/ItemList.js
@@ -1,33 +1,13 @@
 import React from 'react';
 import { Card, Col, Row,Layout,Pagination,Button,Collapse,Cascader,Divider,InputNumber,Input} from 'antd';
-import ItemCard from './ItemCard'
+import ItemCard from './ItemCard';
 const { Panel } = Collapse;
 class ItemList extends React.Component{
     state = {
-        total:3,
+        total:1,
         defaultPageSize:20,
         currentPage:1,
-        info:
-            [
-                {
-                    "uuid":1,
-                    "name":"test1",
-                    "image":'http://image.uc.cn/s/wemedia/s/upload/2020/e3466f09e4bc2b32558be930245a2454.jpg',
-                    "price":19.99
-                },
-                {
-                    "uuid":2,
-                    "name":"test2",
-                    "image":'http://image.uc.cn/s/wemedia/s/upload/2020/e3466f09e4bc2b32558be930245a2454.jpg',
-                    "price":29.99
-                },
-                {
-                    "uuid":3,
-                    "name":"test3",
-                    "image":'http://image.uc.cn/s/wemedia/s/upload/2020/e3466f09e4bc2b32558be930245a2454.jpg',
-                    "price":39.99
-                }
-            ],
+        info:[],
         options:[
             {
               value: 'zhejiang',
@@ -63,10 +43,41 @@ class ItemList extends React.Component{
             },
           ],
         minPrice:-1,
-        maxPrice:0x3FFFFFFFF
+        maxPrice:0x3FFFFFFFF,
+        type:"all",
+        name:"",
     }
+
+    async componentDidMount(){
+        let requestOptions = {
+            method: 'GET',
+            redirect: 'follow'
+        };
+        const data =await fetch("http://localhost:8089/item/listAll?page=1&size=20", requestOptions)
+            .then((response) => {
+                return response.json().then(data => {
+                    if (data.code==200){
+                        //console.log(data.data)
+                        return data.data;
+                    }
+                })
+                
+            })
+            .catch(error => console.log('error', error));
+            this.setState({
+                total:data.total,
+                info:data.list,
+                currentPage:1
+            })
+    }
+
     itemOnClick(key){
-        console.log(key)
+        this.props.history.push({
+            pathname:'../test',
+            state:{
+                'uuid':key
+            }
+        })
     }
     changePage(page,pageSize){
         console.log(page)
@@ -86,7 +97,7 @@ class ItemList extends React.Component{
         const ListCards = info.map((iteminfo)=>
             <Col span={4}>
                 <div onClick={this.itemOnClick.bind(this, iteminfo.uuid)}>
-                    <ItemCard key={iteminfo.uuid} image={iteminfo.image} price={iteminfo.price} name={iteminfo.name}/>
+                    <ItemCard key={iteminfo.uuid} image={JSON.parse(iteminfo.image)[0]} price={iteminfo.price} name={iteminfo.name}/>
                 </div>
             </Col>
         );
@@ -109,7 +120,7 @@ class ItemList extends React.Component{
                         <Col>
                         </Col>
                         <Col>
-                            <p>价格区间</p>
+                            <div>价格区间</div>
                         </Col>
                         <Col>
                             <InputNumber
@@ -143,7 +154,7 @@ class ItemList extends React.Component{
                     total = {total}
                     showTotal={(total, range) => `${range[0]}-${range[1]} of ${total} items`}
                     defaultPageSize={defaultPageSize}
-                    defaultCurrent={current}
+                    defaultCurrent={1}
                     onChange={this.changePage.bind(this)}
                 />
             </div>
