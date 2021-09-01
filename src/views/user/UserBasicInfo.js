4244294bdb312b951a4e5611ff1d9f2ff7f87d0c
wjl <411046642@qq.com>
2021-09-01 19:21:09 +0800

    修改用户基础界面

--- src/views/user/UserBasicInfo.js
+++ src/views/user/UserBasicInfo.js
@@ -1,13 +1,19 @@
 import React from 'react';
-import {Image, Avatar, Button, Space, Col, Rate, List, Divider} from 'antd';
+import {Image, Avatar, Button, Space, Col, Rate, List, Divider, Collapse } from 'antd';
 import { UserOutlined } from '@ant-design/icons';
 import { GetAllOrder } from '../../api/order';
-
+import { Label } from 'semantic-ui-react';
+const { Panel } = Collapse;
 const data = [
     {
         uuid: "23048204",
         paid: true,
         time: 1623080815000, 
+    },
+    {
+        uuid: "23048204",
+        paid: true,
+        time: 1623080815001, 
     }
 ];
 //用户个人页面
@@ -44,24 +50,33 @@ class UserBasicInfo extends React.Component{
                 </Col>
                 <Col>
                     <Divider orientation="left" style={{width: "70%", marginLeft: "5%", marginRight: "5%"}}>我的物流</Divider>
-                    <List
-                    style={{width: "70%", marginLeft: "5%"}}
-                    bordered
-                    dataSource={data}
-                    renderItem={item => (
-                        <List.Item>
-                            <List.Item.Meta
-                                avatar={<Image src={'http://image.uc.cn/s/wemedia/s/upload/2020/e3466f09e4bc2b32558be930245a2454.jpg'} width="50px" height="50px" preview={false} />}
-                                title={<div>商品xxx已发货</div>}
-                                description={<div>"发货时间" {d1}</div>}
-                            />
-                            <Space>
-                                <Button onClick={this.onTest}>查看订单详情</Button>
-                                <Button>确认收货</Button>
-                            </Space>
-                        </List.Item>
-                    )}
-                    />
+                    <Collapse defaultActiveKey={['1']}>
+                        <Panel header="订单xxx" key="1">
+                            <List
+                                style={{width: "90%", marginLeft: "5%"}}
+                                bordered
+                                dataSource={data}
+                                renderItem={item => (
+                                    <List.Item>
+                                        <List.Item.Meta
+                                            avatar={<Image src={'http://image.uc.cn/s/wemedia/s/upload/2020/e3466f09e4bc2b32558be930245a2454.jpg'} width="50px" height="50px" preview={false} />}
+                                            title={<div>商品xxx已发货</div>}
+                                            description={<div>"发货时间" {d1}</div>}
+                                        />
+                                        <Space>
+                                            {/* <Button onClick={this.onTest}>查看订单详情</Button> */}
+                                            <div>数量*2</div>
+                                            <div>价格 199</div>
+                                            <Button type="primary">确认收货</Button>
+                                        </Space>
+                                    </List.Item>
+                                )}
+                                />
+                        </Panel>
+                        <Panel header="订单xxx" key="2">
+
+                        </Panel>
+                    </Collapse>
                 </Col>
             </div>
         );
