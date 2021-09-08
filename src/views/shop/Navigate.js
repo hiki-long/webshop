5b40cbce16fe36a64231c2a65b9ef5db11589caf
wjl <411046642@qq.com>
2021-09-08 23:06:21 +0800

    去除注释

--- src/views/shop/Navigate.js
+++ src/views/shop/Navigate.js
@@ -94,31 +94,6 @@ class Navigate extends React.Component{
                     </Menu>
                 )
                 }
-                {/* <Menu onClick={this.handleClick} selectedKeys={[current]} mode="horizontal">
-                    <Menu.Item key="/">
-                        <Link to='/'>主页</Link>
-                    </Menu.Item>
-                    <Menu.Item key="login">
-                        <Link to="/login">登录</Link>
-                    </Menu.Item>
-                    <Menu.Item>
-                        <Link to="/user">用户中心</Link>
-                    </Menu.Item>
-                    <Menu.Item>
-                        <Link to="/shoppingcart">购物车</Link>
-                    </Menu.Item>
-                    <Menu.Item>
-                        <Link to="/manage">后台管理</Link>
-                    </Menu.Item>
-                    <Menu.Item>
-                        <Link to="/visual">可视化</Link>
-                    </Menu.Item>
-                    <Menu.Item>
-                    {
-                        this.state.haslogin ?  <Button type="primary" onClick={this.handleLogOut}>退出登录</Button> : <></>
-                    }
-                    </Menu.Item>
-                </Menu> */}
             </div>
         );
     }
